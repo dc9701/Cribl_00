9e7c94e43b00353bdcbe7af9b0a6ebf6cba75844
David Cooper <11625114+dc9701@users.noreply.github.com>
2025-12-25 17:05:42 -0800

    Added cleanup to delete events.log

--- test/unit/appCliArgv.test.js
+++ test/unit/appCliArgv.test.js
@@ -1,6 +1,8 @@
-const { spawn } = require('child_process');
 const assert = require('node:assert');
 const test = require('node:test');
+const fs = require('node:fs');
+const path = require('node:path');
+const { spawn } = require('child_process');
 
 test('App should run successfully if VALID ARGV ("agent") is passed', (t, done) => {
   const child = spawn('node', ['app.js', 'agent']);
@@ -59,5 +61,18 @@ test('App should exit w/ status = 1 and display ERROR message if INVALID ARGV is
     assert.strictEqual(code, 1);
     assert.match(output.trim(), /Make sure directory .* exists/);
     done();
+  });
+});
+
+test.after(async () => {
+  // Delete the large output file, if it exists
+  var data = fs.readFileSync('target/outputs.json');
+  var json = JSON.parse(data);
+  var filename = json.file || 'events.log';
+  const outputFile = path.join(process.cwd(), filename);
+  fs.unlink(outputFile, (err) => {
+    if (err) {
+      console.log(`No output file (${outputFile}) to delete; continuing...`);
+    }
   });
 });
